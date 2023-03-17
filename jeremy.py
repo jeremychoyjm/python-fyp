8994f9a0bb0fb965c123d46f7108b3d4e70497ae
Jeremy <88652202+jeremychoyjm@users.noreply.github.com>
2023-03-17 16:27:38 +0800

    Add files via upload

---
+++ jeremy.py
@@ -0,0 +1,8 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("jeremylite.pt")
+results = model.predict(source="0", show=True)  # predict on an image
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

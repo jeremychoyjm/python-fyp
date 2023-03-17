9477300a1b460d0169ba3840ff253af5e389e406
Jeremy <88652202+jeremychoyjm@users.noreply.github.com>
2023-03-17 16:32:05 +0800

    Update jeremy.py

--- jeremy.py
+++ jeremy.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO("jeremylite.pt")
+model = YOLO("jeremy.pt")
 results = model.predict(source="0", show=True)  # predict on an image
 
 cv2.waitKey(0)

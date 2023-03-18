adf60fcae16a7087e5ca30432c878393348c6469
Jeremy <88652202+jeremychoyjm@users.noreply.github.com>
2023-03-18 12:09:50 +0800

    Update jeremyfinal.py

--- jeremyfinal.py
+++ jeremyfinal.py
@@ -2,7 +2,7 @@ import cv2
 from ultralytics import YOLO
 
 model = YOLO("jeremy.pt")
-results = model.predict(source="0", show=True)  # predict on an image
+results = model.predict(source="0", show=True)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
